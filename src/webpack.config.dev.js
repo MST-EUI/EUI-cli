73d2daa2d090bb476e104cc99278ae03f084a099
yukap <wb-jwr216045@alibaba-inc.com>
2018-07-23 10:04:44 +0800

    remove external for webpack.dev

--- src/webpack.config.dev.js
+++ src/webpack.config.dev.js
@@ -14,22 +14,6 @@ module.exports = {
     library: 'demo',
     libraryTarget: 'umd',
   },
-  externals: {
-    react: {
-      root: 'React',
-      commonjs2: 'react',
-      commonjs: 'react',
-      amd: 'react',
-      umd: 'react',
-    },
-    'react-dom': {
-      root: 'ReactDOM',
-      commonjs2: 'react-dom',
-      commonjs: 'react-dom',
-      amd: 'react-dom',
-      umd: 'react-dom',
-    },
-  },
   cache: true,
   module: {
     loaders: [
